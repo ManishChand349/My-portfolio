81d7cbbc86ecde98e6d160f6416ead57c4c8c692
Manish Chand <manishchandr349@gmail.com>
2023-07-20 11:08:00 +0530

    adding routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route , Link, Routers } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Navbar/>
+       <Router>
+        <Routers>
+          <Route exect path="/" element={<Navbar/>}/>
+        </Routers>
+       </Router>
       </header>
     </div>
   );
