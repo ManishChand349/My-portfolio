12bfc1320c115ccbe1e80b6ab298dad3c10eb092
Manish Chand <manishchandr349@gmail.com>
2023-07-20 11:25:18 +0530

    router done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Route , Link, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar/>,
+  },
+]);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <Router>
-        <Routes>
-          <Route exect path="/" element={<Navbar/>}/>
-        </Routes>
-       </Router>
-      </header>
-    </div>
+    <>
+       <RouterProvider router={router} />
+    </>
   );
 }
 
